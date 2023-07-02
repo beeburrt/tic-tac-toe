09b1212970ee845e9843a31d48ca3c79ee8ecac1
Brian Burt <beeburrt@outlook.com>
2023-07-02 00:01:40 -0700

    more config

--- init.rb
+++ init.rb
@@ -1,23 +1,10 @@
 # frozen_string_literal: true
-  require "io/console"
 
-  # must use parens here, see:
-  # https://stackoverflow.com/questions/3170553/how-can-i-clear-the-terminal-in-ruby
-  #system("clear") || system("cls") # for portability (cls = Windows)
-  #puts "\e[H\e[2J" 
-  #$stdout.clear_screen # clears the screen, same as above 2
-  #puts "\n\tWelcome to Tic Tac Toe\n\n"
+require 'io/console'
+require_relative 'ttt'
 
-#   counter = 0
-#   until counter == 50
-#     puts " "
-#     counter += 1
-#   end
-
-# puts "Enter your password: "
-# password = STDIN.noecho(&:gets).chomp # noecho hides output
-# puts "your password is #{password}"
-
-
-puts "\e[H\e[2J"
-STDOUT.flush
+# must use parens here, see:
+# https://stackoverflow.com/questions/3170553/how-can-i-clear-the-terminal-in-ruby
+system('clear') || system('cls') # for portability (cls = Windows)
+# puts "\e[H\e[2J"
+puts "\n\tWelcome to Tic Tac Toe\n\n"
---
+++ ttt.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+parser = OptionParser.new
